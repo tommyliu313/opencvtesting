cc95ebeb3e03415ec96e3ddf397741b2e0ff4e04
tommyliu313 <liuchaktin@gmail.com>
2022-10-31 23:19:20 +0800

    20221031 2319

--- detect/faces.py
+++ detect/faces.py
@@ -3,4 +3,16 @@ import numpy as np
 
 # loading haar cascade xml files
 
-image = cv2.CascadeClassifier('./haar_cascade/haarcascade_frontalcatface.xml')
+classifier = cv2.CascadeClassifier('./haar_cascade/haarcascade_frontalcatface.xml')
+
+image = cv2.imread('testingtarget/family.jpg')
+
+# detectmultiface
+
+for (x1,x2,y1,y2) :
+
+    cv2.rectangle()
+
+
+cv2.imshow("original",image);
+cv2.imshow("after",after)
