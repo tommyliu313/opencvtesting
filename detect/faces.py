f568516fae2138fe8b03d88de51fe63ed0695063
tommyliu313 <liuchaktin@gmail.com>
2022-11-02 01:02:22 +0800

    20220211 0102

--- detect/faces.py
+++ detect/faces.py
@@ -4,23 +4,30 @@ import numpy as np
 # setting
 color = (0,0,255)
 
+color2 = (255,0,255)
+
 # loading haar cascade xml files
 
 classifier = cv2.CascadeClassifier('./haar_cascade/haarcascade_frontalcatface.xml')
 
 image = cv2.imread('testingtarget/family.jpg')
 
-
+#scenario 1: recognize people face
+#process involved: face detection
+#using function:
 
 for (x,y,width,height) in image :
 
-    cv2.rectangle(image,(x,y),(x+width,y+height),)
+    cv2.rectangle(image,(x,y),(x+width,y+height),color2,)
 
 # detectmultiface
-checkmultiface = classifier.detectmultiface()
+checkmultiface = classifier.detectMultiScale()
 # name the window
 
 cv2.imshow("original",image)
 cv2.imshow("after",after)
 
-#conver
+#scenario 2:
+#process involved:
+#using function: 
+#description:
